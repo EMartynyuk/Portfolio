7b86a5187ab8f50771f6e1af243180d513c3ec07
EMartynyuk <martynyukei21@gmail.com>
2023-05-21 22:09:51 +0300

    change main page + adap

--- app/js/main.js
+++ app/js/main.js
@@ -1,21 +1,30 @@
-$(function(){
-    
+$(function () {
+   
+  $('.header__btn').on('click', function(){
+    $('.header__lists').toggleClass('header__lists--active')
+});
 
-    var tiker = $(".slider__item");
-   tiker.css({"overflow": "hidden", "width": "100%"});
 
-   tiker.wrapInner("<span>");
-   tiker.find("span").css({ "width": "50%", "display": "inline-block", "text-align":"center" });
-   tiker.append(tiker.find("span").clone()); 
 
-   tiker.wrapInner("<div>");
-   tiker.find("div").css("width", "360%");
 
-   var reset = function() {
+
+
+  var tiker = $(".slider__item");
+  tiker.css({ overflow: "hidden", width: "100%" });
+
+  tiker.wrapInner("<span>");
+  tiker
+    .find("span")
+    .css({ width: "50%", display: "inline-block", "text-align": "center" });
+  tiker.append(tiker.find("span").clone());
+
+  tiker.wrapInner("<div>");
+  tiker.find("div").css("width", "650%");
+
+  var reset = function () {
     $(this).css("margin-left", "0%");
-    $(this).animate({ "margin-left": "-100%" }, 6000, 'linear', reset);
-   };
+    $(this).animate({ "margin-left": "-200%" }, 8000, "linear", reset);
+  };
 
-   reset.call(tiker.find("div"));
-
-})
+  reset.call(tiker.find("div"));
+});
